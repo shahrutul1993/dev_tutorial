d0ca39d17427a7a63aeba608bae0c0dcdbd5d048
Rutul Shah <shahrutul1993@github.com>
2017-04-17 19:33:55 +0530

    implement deafult setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,16 +12,16 @@ class PortfoliosController < ApplicationController
 	end	
 
 	def create
-	@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
-      if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
-      else
-        format.html { render :new }
-      end
-    end
-	end	
+	    respond_to do |format|
+	      if @portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+	      else
+	        format.html { render :new }
+	      end
+	    end
+	end
 
 	def edit
 		@portfolio_item = Portfolio.find(params[:id])
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,14 @@
 class Portfolio < ApplicationRecord
-	validates_presence_of :title, :body, :main_image, :thumb_image
+	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
+	end	
+
+	after_initialize :set_deafults 
+
+	def set_deafults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "://placehold.it/350x200"
 	end	
 end
