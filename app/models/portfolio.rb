d1be06ad16aeb99aee88fd55cafa1eaac41c16ea
Rutul Shah <shahrutul1993@github.com>
2017-04-17 21:08:34 +0530

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,14 +1,15 @@
 class Portfolio < ApplicationRecord
+	include  Placeholder
 	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
 	def self.angular
 		where(subtitle: 'Angular')
-	end	
+	end
 
-	after_initialize :set_deafults 
+	after_initialize :set_deafults
 
 	def set_deafults
-		self.main_image ||= "http://placehold.it/600x400"
-		self.thumb_image ||= "://placehold.it/350x200"
-	end	
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+	end
 end
