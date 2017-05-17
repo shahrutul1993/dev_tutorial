93a0193e41fbe191eadfaed1b0fbe156753031a2
Rutul Shah <shahrutul1993@github.com>
2017-05-18 00:33:32 +0530

    added method for copyright in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  include DeviseWhitelist
-  include SetSource
-  include CurrentUserConcern
-  include DefaultPageContent
+	protect_from_forgery with: :exception
+	include DeviseWhitelist
+	include SetSource
+	include CurrentUserConcern
+	include DefaultPageContent
+
+
+	before_action :set_copyright
+
+	def set_copyright
+		@copyright = RutulViewTool::Renderer.copyright 'Rutul Shah', 'All rights reserved'
+	end
+end
+
+module RutulViewTool
+	class  Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
+
 end
